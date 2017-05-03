73e918145b26d52f2fa33feaddbaa07a186b48a3
Papa <kub1@ukr.net>
2017-05-03 09:32:07 +0300

    test commit

--- app/src/main/java/com/example/papa/test/MainActivity.java
+++ app/src/main/java/com/example/papa/test/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1111111111111111
+///111111111
     }
 }
